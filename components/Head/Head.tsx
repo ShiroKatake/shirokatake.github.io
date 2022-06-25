954f8f5d629250ab255156af4ff9e123b63ac6c3
ShiroKatake <tgkhang262@gmail.com>
2022-06-26 09:30:26 +1000

    remove: redudant head component, might need it later but not now

--- components/Head/Head.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-import NextHead from "next/head";
-
-export const Head:React.FC = () => {
-  return (
-    <NextHead>
-    </NextHead>
-  )
-}
--- components/index.tsx
+++ components/index.tsx
@@ -1,5 +1,4 @@
 import { Grid, Row, Col } from "./FlexboxGrid/FlexboxGrid";
-import { Head } from "./Head/Head";
 import { NavBar } from "./NavBar/NavBar";
 
-export { Grid, Row, Col, Head, NavBar };
+export { Grid, Row, Col, NavBar };
