a2ac237233e0c01ff7fb3fb326046692c4ea098b
ShiroKatake <tgkhang262@gmail.com>
2022-06-26 10:22:27 +1000

    fix(NavBar styling): removed flexboxgrid, looks better on desktop

--- components/NavBar/NavBar.tsx
+++ components/NavBar/NavBar.tsx
@@ -1,25 +1,19 @@
-import React from 'react'
-import { Col, Grid, Row } from '../FlexboxGrid/FlexboxGrid'
-import { StyledHeader, StyledNavList } from './NavBar.styled'
-import { NavItem } from './subcomponents/NavItem'
+import React from 'react';
+import { Col, Grid, Row } from '../FlexboxGrid/FlexboxGrid';
+import { StyledHeader, StyledNavList } from './NavBar.styled';
+import { NavItem } from './subcomponents/NavItem';
 
 export const NavBar:React.FC = () => {
   return (
     <StyledHeader>
-      <Grid>
-        <Row>
-          <Col xs={4} sm={8} md={12}>
-            <nav>
-              <StyledNavList>
-                <NavItem primary href='/'>Khang Trinh</NavItem>
-                <NavItem href='/projects'>Projects</NavItem>
-                <NavItem href='/resume'>Resume</NavItem>
-                <NavItem href='/contact'>Contact</NavItem>
-              </StyledNavList>
-            </nav>
-          </Col>
-        </Row>
-      </Grid>
+      <nav>
+        <StyledNavList>
+          <NavItem primary href='/'>Khang Trinh</NavItem>
+          <NavItem href='/projects'>Projects</NavItem>
+          <NavItem href='/resume'>Resume</NavItem>
+          <NavItem href='/contact'>Contact</NavItem>
+        </StyledNavList>
+      </nav>
     </StyledHeader>
   )
 }
