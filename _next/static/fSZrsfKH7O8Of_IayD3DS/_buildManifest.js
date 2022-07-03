d583ca0d79b3f668356c9c58c5ec29985e667efb
ShiroKatake <ShiroKatake@users.noreply.github.com>
2022-07-03 10:47:19 +0000

    deploy: fdfc21763cc871ce2f0434247d8db32dcb166270

---
+++ _next/static/fSZrsfKH7O8Of_IayD3DS/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/959-9ca386803b6293ef.js","static/chunks/pages/index-ed68063a695c8233.js"],"/_error":["static/chunks/pages/_error-777a5f0e462a9bdf.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
